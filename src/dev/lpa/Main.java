1a71105367fea403e8d645005d7b5e581db8204f
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-29 19:48:14 +0100

    add method to print records

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -3,6 +3,8 @@ package dev.lpa;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
@@ -24,9 +26,36 @@ public class Main {
     try (Connection connection = dataSource.getConnection(
       System.getenv("MYSQL_USER"),
       System.getenv("MYSQL_PASS"))) {
-
+      
+      String sql = "SELECT * FROM music.albumview WHERE artist_name = ?";
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ps.setString(1, "Elf");
+      ResultSet resultSet = ps.executeQuery();
+      printRecords(resultSet);
     } catch (SQLException e) {
       e.printStackTrace();
     }
+  }
+  
+  private static boolean printRecords(ResultSet resultSet) throws SQLException {
+    
+    boolean foundData = false;
+    var meta = resultSet.getMetaData();
+    
+    System.out.println("==============================");
+    
+    for (int i = 1; i <= meta.getColumnCount(); i++) {
+      System.out.printf(i == 1 ? "%-30s" : "%-15s", meta.getColumnName(i).toUpperCase());
+    }
+    System.out.println();
+    
+    while (resultSet.next()) {
+      for (int i = 1; i <= meta.getColumnCount(); i++) {
+        System.out.printf(i == 1 ? "%-30s" : "%-15s", resultSet.getString(i));
+      }
+      System.out.println();
+      foundData = true;
+    }
+    return foundData;
   }
 }
