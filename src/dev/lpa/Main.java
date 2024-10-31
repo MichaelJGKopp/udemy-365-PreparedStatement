aa0855dc24be46c5dcf88f114fcd9efdb74d3690
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-31 10:32:24 +0100

    call ddDataFromFile for Bob Dylan, fix typeo

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -13,7 +13,7 @@ public class Main {
   private static String ARTIST_INSERT =
     "INSERT INTO music.artists (artist_name) VALUES (?)";
   private static String ALBUM_INSERT =
-    "INSERT INTO music.albums (arist_id, album_name) VALUES (?, ?)";
+    "INSERT INTO music.albums (artist_id, album_name) VALUES (?, ?)";
   private static String SONG_INSERT =
     "INSERT INTO music.songs (album_id, track_number, song_title) VALUES (?, ?, ?)";
   
@@ -33,11 +33,13 @@ public class Main {
     
     try (Connection connection = dataSource.getConnection(
       System.getenv("MYSQL_USER"),
-      System.getenv("MYSQL_PASS"))) {
+      System.getenv("MYSQL_PASS"))
+    ) {
+      addDataFromFile(connection);
       
       String sql = "SELECT * FROM music.albumview WHERE artist_name = ?";
       PreparedStatement ps = connection.prepareStatement(sql);
-      ps.setString(1, "Elf");
+      ps.setString(1, "Bob Dylan");
       ResultSet resultSet = ps.executeQuery();
       printRecords(resultSet);
     } catch (SQLException e) {
